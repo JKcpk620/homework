d3401ac014f79910cfc4ed24f31a189ded6980a0
JKcpk620 <real9siu@gmail.com>
2026-07-29 23:50:12 +0800

    feat fnb_choices

---
+++ f_and_b/choices.py
@@ -0,0 +1,44 @@
+f_or_b_choices = {
+    'Foods' : 'Foods',
+    'Beverages' : 'Beverages',
+}
+
+cooking_type_choices = {
+    'Baking' : 'Baking',
+    'Roasting' : 'Roasting',
+    'Grilling' : 'Grilling',
+    'Frying' : 'Frying',
+    'Boiling' : 'Boiling',
+    'Steaming' : 'Steaming',
+    'Braising' : 'Braising',
+    'Pan_Frying' : 'Pan_Frying', 
+    'Stir_Frying' : 'Stir_Frying',
+    'Stewing' : 'Stewing',
+    'Slow_Cooking' : 'Slow_Cooking',
+    'Salad' : 'Salad',
+    'Cold_Dish' : 'Cold_Dish',
+    'Dessert' : 'Dessert',
+    'Soup_Noodle' : 'Soup_Noodle',
+    'Others' : 'Others',
+}
+
+Beverage_choices = {
+    'Cold_Drinks' : 'Cold_Drinks',
+    'Hot_Drinks' : 'Hot_Drinks',
+    'Water' : 'Water',
+    'Soft_Drinks' : 'Soft_Drinks',
+    'Juices' : 'Juices',
+    'Coffee' : 'Coffee',
+    'Tea' : 'Tea',
+    'Milkshakes' : 'Milkshakes',
+    'Smoothies' : 'Smoothies',
+    'Leung_Cha' : 'Leung_Cha',
+    'Cocktails' : 'Cocktails',
+    'Mocktails' : 'Mocktails',
+    'Alcoholic_Drinks' : 'Alcoholic_Drinks',
+    'Beers' : 'Beers',
+    'Wines' : 'Wines',
+    'Spirits' : 'Spirits',
+    'Functional_Drinks' : 'Functional_Drinks',
+    'Others' : 'Others',
+}
--- merchants/models.py
+++ merchants/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Merchant(models.Model):
+class Merchants(models.Model):
     title = models.CharField(max_length=200)
     district = models.CharField(max_length=50)
     address = models.CharField(max_length=200)
